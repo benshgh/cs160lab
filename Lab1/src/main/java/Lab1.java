5778585201046366b9870d32c8cb94dcc3031f21
benshgh <81721129+benshgh@users.noreply.github.com>
2021-08-20 01:58:06 -0700

    Formatting changes

--- Lab1/src/main/java/Lab1.java
+++ Lab1/src/main/java/Lab1.java
@@ -1,67 +1,65 @@
 import java.util.*;
 
 class Lab1 {
-	
-	//Sort carInventory based on the order in promotionOrder
-	 public static String[] sortCarInventory(String[] carInventory, String[] promotionOrder) {
 
-       String[] sortedCarInventory = new String[carInventory.length];
+	// Sort carInventory based on the order in promotionOrder
+	public static String[] sortCarInventory(String[] carInventory, String[] promotionOrder) {
 
-       int index = 0;
+		String[] sortedCarInventory = new String[carInventory.length];
 
-       //Loop for each item in the promotion order
-       for (int pIndex = 0; pIndex < promotionOrder.length; pIndex ++) {
-           //loop for each car in the carInventory list
-           for (String c : carInventory) {
-               if (c.equals(promotionOrder[pIndex])) {
-                   sortedCarInventory[index] = c;
-                   index++;
-               }
-           }
-       }
-       return sortedCarInventory;
-   }
+		int index = 0;
 
-   public static void main(String[] args) {
+		// Loop for each item in the promotion order
+		for (int pIndex = 0; pIndex < promotionOrder.length; pIndex++) {
+			// loop for each car in the carInventory list
+			for (String c : carInventory) {
+				if (c.equals(promotionOrder[pIndex])) {
+					sortedCarInventory[index] = c;
+					index++;
+				}
+			}
+		}
+		return sortedCarInventory;
+	}
 
-   	//get promotion order from command line arguments
-   	ArrayList<String> promotionOrderFromArgs = new ArrayList<String>();
-   	for (int i = 0; i < args.length; i++) {
-           promotionOrderFromArgs.add(args[i]);
-       }
-   	
-       //Test sorting car inventory based on a promotion order
-       String[] carInventory = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mx"};
-       
+	public static void main(String[] args) {
 
-       String[] sortedList  = {};
-       String[] promotionOrder = {"mz", "mx", "my"}; //default promotion order
-       if(!promotionOrderFromArgs.isEmpty()) {
-       	promotionOrder = promotionOrderFromArgs.toArray(new String[0]);
-       	sortedList = Lab1.sortCarInventory(carInventory, promotionOrder);
-       }
-       else {
-       	sortedList = Lab1.sortCarInventory(carInventory, promotionOrder);
-       }
-       
-       System.out.print("Car promotion order: ");
-       for (String s : promotionOrder) {
-           System.out.print(s  + " ");
-       }
-       
-       System.out.print("\n");
-       
-       System.out.print("Car inventory before sorting: ");
-       for (String s : carInventory) {
-           System.out.print(s  + " ");
-       }
-       
-       System.out.print("\n");
-       
-       System.out.print("Car inventory after sorting based on promotion: ");
-       for (String s : sortedList) {
-           System.out.print(s  + " ");
-       }
-   }
+		// get promotion order from command line arguments
+		ArrayList<String> promotionOrderFromArgs = new ArrayList<String>();
+		for (int i = 0; i < args.length; i++) {
+			promotionOrderFromArgs.add(args[i]);
+		}
+
+		// Test sorting car inventory based on a promotion order
+		String[] carInventory = { "mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mx" };
+
+		String[] sortedList = {};
+		String[] promotionOrder = { "mz", "mx", "my" }; // default promotion order
+		if (!promotionOrderFromArgs.isEmpty()) {
+			promotionOrder = promotionOrderFromArgs.toArray(new String[0]);
+			sortedList = Lab1.sortCarInventory(carInventory, promotionOrder);
+		} else {
+			sortedList = Lab1.sortCarInventory(carInventory, promotionOrder);
+		}
+
+		System.out.print("Car promotion order: ");
+		for (String s : promotionOrder) {
+			System.out.print(s + " ");
+		}
+
+		System.out.print("\n");
+
+		System.out.print("Car inventory before sorting: ");
+		for (String s : carInventory) {
+			System.out.print(s + " ");
+		}
+
+		System.out.print("\n");
+
+		System.out.print("Car inventory after sorting based on promotion: ");
+		for (String s : sortedList) {
+			System.out.print(s + " ");
+		}
+	}
 
 }
