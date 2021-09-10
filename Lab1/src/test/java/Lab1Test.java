1d6d3718ffe3a7a52d15c1afd742705a5b9f551d
benshgh <81721129+benshgh@users.noreply.github.com>
2021-09-10 14:37:26 -0700

    Added new files for Lab 3

--- Lab1/src/test/java/Lab1Test.java
+++ Lab1/src/test/java/Lab1Test.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 
 public class Lab1Test {
 
+	String[] promotionOrder = new String[] { "mz", "mx", "my" };
+	
 	void assertResults(String[] expected, String[] actual) {
 		assertTrue(expected.length == actual.length);
 		for (int i = 0; i < expected.length; i++) {
@@ -15,7 +17,6 @@ public class Lab1Test {
 	@Test
 	public void TestCase1() {
 		String[] carInventory = new String[] { "mz", "my", "my", "mx", "mz", "mx", "my", "mz" };
-		String[] promotionOrder = new String[] { "mz", "mx", "my" };
 		String[] expectedRes = new String[] { "mz", "mz", "mz", "mx", "mx", "my", "my", "my" };
 		String[] actualRes = Lab1.sortCarInventory(carInventory, promotionOrder);
 		assertResults(expectedRes, actualRes);		
@@ -24,7 +25,6 @@ public class Lab1Test {
 	@Test
 	public void TestCase2() {
 		String[] carInventory = new String[] { "mx", "mz" };
-		String[] promotionOrder = new String[] { "mz", "mx", "my" };
 		String[] expectedRes = new String[] { "mz", "mx" };
 		String[] actualRes = Lab1.sortCarInventory(carInventory, promotionOrder);
 		assertResults(expectedRes, actualRes);
@@ -33,7 +33,6 @@ public class Lab1Test {
 	@Test
 	public void TestCase3() {
 		String[] carInventory = new String[] { "mx" };
-		String[] promotionOrder = new String[] { "mz", "mx", "my" };
 		String[] expectedRes = new String[] { "mx" };
 		String[] actualRes = Lab1.sortCarInventory(carInventory, promotionOrder);
 		assertResults(expectedRes, actualRes);
